a7876a34d41b5c4046d27609ee0a41154f96d6fa
JunHoJang <jjjh3369@naver.com>
2023-12-05 11:44:39 +0900

    9

--- HTML/js/map.js
+++ HTML/js/map.js
@@ -1,15 +1,14 @@
 
- function mapMenuClick(e){
-    document.querySelector('#mapMenu').innerHTML = e.innerHTML;
+function mapMenuClick(e) {
+	document.querySelector('#mapMenu').innerHTML = e.innerHTML;
 
-		var selall = document.querySelectorAll('.tempNum');
-		for(let i = 0; i < selall.length; i++){
-			selall[i].innerHTML = e.innerHTML;
-		}
+	var selall = document.querySelectorAll('.tempNum');
+	for (let i = 0; i < selall.length; i++) {
+		selall[i].innerHTML = e.innerHTML;
+	}
 }
 
- 
- function ClickPopUpBtn(e){
-    document.querySelector('#PopUpArea').innerHTML = e.innerHTML;
- }
+function ClickPopUpBtn(e) {
+	document.querySelector('#PopUpArea').innerHTML = e.innerHTML;
+}
 
